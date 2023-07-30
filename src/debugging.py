041e601a11f5b6ff2022cacae0bfc8f161f32f8f
basalganglia <ilkka.kosunen@gmail.com>
2023-07-30 14:46:13 +0000

    copilot comments

--- src/debugging.py
+++ src/debugging.py
@@ -1,5 +1,17 @@
 """
-This module allows us to have a centralized logger that will be used from multiple processes at the same time.    
+This module allows us to have a centralized logger that will be used from multiple processes at the same time.
+
+
+Here is copilot explanation:
+
+Here is the explanation for the code below:
+1. get_logger() returns a logger object with the name of the current module.
+2. setLevel() sets the threshold for this logger to level.
+3. StreamHandler() returns a handler object that can be used to log messages to the console.
+4. setFormatter() sets the Formatter for this handler to fmt.
+5. Because logging.Formatter doesn't have any arguments, we use the default arguments.
+
+
 """
 
 from multiprocessing import get_logger
