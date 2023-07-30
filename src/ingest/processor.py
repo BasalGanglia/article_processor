7b4de571e7584ebc7ac05f56e4327de09ba7af7e
basalganglia <ilkka.kosunen@gmail.com>
2023-07-30 14:49:22 +0000

    comment prompt

---
+++ src/ingest/processor.py
@@ -0,0 +1,7 @@
+"""
+this module enables us to process text using natural language processing
+to extract known entities - i.e. nouns
+
+It uses Spacy to extract entities based on a pre-built model.
+The model must be downloaded before using spacy
+"""
