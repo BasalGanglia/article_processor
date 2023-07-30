b013882b21bc865e0314fbac54b74edb079bf35f
basalganglia <ilkka.kosunen@gmail.com>
2023-07-30 12:17:50 +0000

    playing around with AI comment snippets

--- src/debugging.py
+++ src/debugging.py
@@ -5,13 +5,23 @@ This module allows us to have a centralized logger that will be used from multip
 from multiprocessing import get_logger
 import logging
 
-def(level=logging.INFO): -> logging.Logger
+
+def logger(level=logging.INFO) -> logging.Logger:
     """
     Returns a logger that can be used from multiple processes at the same time.
     """
+    """
+    .. note:: AI Comments
+    
+    q: what is the purpose of this function?
+    a: This function returns a logger that can be used from multiple processes at the same time.
+    q: What is the purpose of StreamHander()?
+    a: StreamHandler() returns a handler object that can be used to log messages to the console.
+    """
     logger = get_logger()
     logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
     return logger
-
-
-# Exposing app_logger to be used by other modules.
